f25ca5cecfdfcbb4986d58bb58eea397a15e7b0c
HugoDelval <hugodelval@gmail.com>
2016-11-11 17:22:41 +0000

    cleaner kill, kill still doesn't work on nebula + docker

--- app/Main.hs
+++ app/Main.hs
@@ -22,32 +22,38 @@ main = do
     listen sock (nbThreads*2)                                  -- queue of 10 connections max
     withPool nbThreads $ 
         \pool -> parallel_ pool (replicate nbThreads (mainLoop sock port))
+    putStrLn "Server killed. See you !"
     
 mainLoop :: Socket -> String -> IO ()
 mainLoop sock port = do
+    let killed = False
     putStrLn "Waiting for incoming connection..."
     conn <- try (accept sock) :: IO (Either SomeException (Socket, SockAddr))  -- try to accept a connection and handle it
     case conn of
         Left  _    -> putStrLn "Socket is now closed. Exiting."
-        Right conn -> putStrLn "Got a client !" >>
-                      runConn conn sock port    >>  -- run our server's logic, then
-                      mainLoop sock port            -- repeat
+        Right conn -> do
+            putStrLn "Got a client !"
+            killed <- runConn conn sock port                   -- run our server's logic, then
+            if killed then return () else mainLoop sock port   -- repeat
 
-runConn :: (Socket, SockAddr) -> Socket -> String -> IO ()
+runConn :: (Socket, SockAddr) -> Socket -> String -> IO Bool
 runConn (sock, addr) originalSocket port = do
+    let killed = False
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl LineBuffering
-    handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
+    killed <- handle (\(SomeException _) -> return False) $ fix $ \loop -> do
         line <- hGetLine hdl
         let commandAndArgs = splitOn " " line
         let command = head commandAndArgs
         let args = intercalate " " $ tail commandAndArgs 
         case command of
-            "KILL_SERVICE" -> killService originalSocket
+            "KILL_SERVICE" -> do
+                killService originalSocket
+                return True
             "HELO"         -> helo hdl addr args port >> loop
             _              -> otherCommand hdl line   >> loop
-    
     hClose hdl
+    return killed
 
 sendResponse :: Handle -> String -> IO ()
 sendResponse hdl resp = do
@@ -58,9 +64,8 @@ getHostNameIfDockerOrNot :: IO String
 getHostNameIfDockerOrNot = do
     weAreInDocker <- doesFileExist "/.dockerenv"
     host <- if weAreInDocker 
-    	then getHostByName "dockerhost" 
-    	else (getHostName >>= getHostByName)
-    putStrLn $ show $ hostName host
+        then getHostByName "dockerhost" 
+        else (getHostName >>= getHostByName)
     return $ show $ fromHostAddress $ head $ hostAddresses host
 
 killService :: Socket -> IO ()
@@ -72,10 +77,9 @@ helo :: Handle -> SockAddr -> String -> String -> IO ()
 helo hdl addr text port = do
     putStrLn $ "Responding to HELO command with params : " ++ text
     hostname <- getHostNameIfDockerOrNot
-    let resp = "HELO " ++ text ++ "\nIP:" ++ hostname ++ "\nPort:" ++ port ++ "\nStudentID:16336620"
-    putStrLn $ resp
-    sendResponse hdl $ resp
+    sendResponse hdl $ "HELO " ++ text ++ "\nIP:" ++ hostname ++ "\nPort:" ++ port ++ "\nStudentID:16336620"
 
 otherCommand :: Handle -> String -> IO ()
 otherCommand hdl param = do
+    putStrLn $ "Received unknown query : " ++ param
     sendResponse hdl $ "Command not implemented yet : " ++ param ++ "\nStay tuned !"

7c5fc3558ec3613ae60e74a0eaa328ad9d161e9c
HugoDelval <hugodelval@gmail.com>
2016-11-17 16:31:57 +0000

    KILL_SERVICE -> kill alive clients (and threads)

--- app/Main.hs
+++ app/Main.hs
@@ -8,9 +8,15 @@ import System.Directory
 import Control.Concurrent.ParallelIO.Local
 import Control.Exception
 import Control.Monad.Fix (fix)
+import Control.Concurrent.Chan
 import Data.List
 import Data.IP
+import Foreign.Ptr
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Marshal.Alloc (mallocBytes, free)
 import Data.List.Split
+import Control.Monad (when)
 
 main :: IO ()
 main = do
@@ -20,40 +26,58 @@ main = do
     bind sock (SockAddrInet (toEnum $ read port) iNADDR_ANY)   -- listen on TCP port given by user.
     let nbThreads = 5
     listen sock (nbThreads*2)                                  -- queue of 10 connections max
+    chan <- newChan
     withPool nbThreads $ 
-        \pool -> parallel_ pool (replicate nbThreads (mainLoop sock port))
+        \pool -> parallel_ pool (replicate nbThreads (mainLoop sock port chan))
     putStrLn "Server killed. See you !"
     
-mainLoop :: Socket -> String -> IO ()
-mainLoop sock port = do
-    let killed = False
+mainLoop :: Socket -> String -> Chan Bool -> IO ()
+mainLoop sock port chan = do
     putStrLn "Waiting for incoming connection..."
     conn <- try (accept sock) :: IO (Either SomeException (Socket, SockAddr))  -- try to accept a connection and handle it
     case conn of
         Left  _    -> putStrLn "Socket is now closed. Exiting."
         Right conn -> do
             putStrLn "Got a client !"
-            killed <- runConn conn sock port                   -- run our server's logic, then
-            if killed then return () else mainLoop sock port   -- repeat
+            runConn conn sock port chan  -- run our server's logic, then
+            mainLoop sock port chan      -- repeat
 
-runConn :: (Socket, SockAddr) -> Socket -> String -> IO Bool
-runConn (sock, addr) originalSocket port = do
-    let killed = False
+runConn :: (Socket, SockAddr) -> Socket -> String -> Chan Bool -> IO ()
+runConn (sock, addr) originalSocket port chan = do
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl LineBuffering
-    killed <- handle (\(SomeException _) -> return False) $ fix $ \loop -> do
-        line <- hGetLine hdl
-        let commandAndArgs = splitOn " " line
+    handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
+        (kill, line) <- waitForInput hdl chan
+        when kill (return ())
+        let commandAndArgs = splitOn " " (init line)
         let command = head commandAndArgs
         let args = intercalate " " $ tail commandAndArgs 
         case command of
             "KILL_SERVICE" -> do
                 killService originalSocket
-                return True
+                writeChan chan True
             "HELO"         -> helo hdl addr args port >> loop
             _              -> otherCommand hdl line   >> loop
     hClose hdl
-    return killed
+
+waitForInput :: Handle -> Chan Bool -> IO (Bool, String)
+waitForInput hdl chan = do
+  stillAlive <- isEmptyChan chan
+  if stillAlive
+    then do
+      request <- handle (\(SomeException _) -> return "") $ fix $ \getReq -> do
+        buf <- mallocBytes 4096 :: IO (Ptr CChar)
+        nbRead <- hGetBufNonBlocking hdl buf 4096 
+        request <- peekCStringLen (buf, nbRead)
+        free buf
+        return request
+      if null request
+        then do
+          res <- waitForInput hdl chan
+          return res
+        else do
+          return (False, request)
+    else return (True, [])
 
 sendResponse :: Handle -> String -> IO ()
 sendResponse hdl resp = do
